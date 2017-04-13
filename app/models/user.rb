6afe76fa51fa840897609e2fd512274182bad8d0
Julia(e) Xu <email": null>
2017-04-13 15:33:27 -0700

    Form associations and beginning relationships between user and coffeeshops

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :coffeeshops
+
+  has_secure_password
 end
--- db/migrate/20170413144955_create_coffeeshops.rb
+++ db/migrate/20170413144955_create_coffeeshops.rb
@@ -1,4 +1,10 @@
 class CreateCoffeeshops < ActiveRecord::Migration
   def change
+    create_table :coffeeshops do |t|
+      t.string :name, null: false
+      t.location :name, null: false
+
+      t.timestamps null: false
+    end
   end
 end
