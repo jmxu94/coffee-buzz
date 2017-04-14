a1e0afb02f9be73c470acbf7d8ecee0c3875de9f
Julia(e) Xu <email": null>
2017-04-14 16:24:07 -0700

    Add coffeeshop to front page upon buzz

--- app/controllers/coffeeshops.rb
+++ app/controllers/coffeeshops.rb
@@ -1,15 +1,20 @@
-get '/coffeeshops' do
-  if Coffeeshop.all.length > 0
-    Coffeeshop.all.each do |coffeeshop|
-      coffeeshop.destroy
-    end
-    ActiveRecord::Base.connection.reset_pk_sequence!('coffeeshops')
-  end
-  coffeeshop = Coffeeshop.create(search(params['location']))
-  redirect "/coffeeshops/#{coffeeshop.id}"
-end
+# get '/coffeeshops' do
+#   if Coffeeshop.all.length > 0
+#     Coffeeshop.all.each do |coffeeshop|
+#       coffeeshop.destroy
+#     end
+#     ActiveRecord::Base.connection.reset_pk_sequence!('coffeeshops')
+#   end
+#   coffeeshop = Coffeeshop.create(search(params['location']))
+#   redirect "/coffeeshops/#{coffeeshop.id}"
+# end
 
-get '/coffeeshops/:id' do
-  @coffeeshop = Coffeeshop.find(params[:id])
-  erb :'/coffeeshops/show'
+# get '/coffeeshops/:id' do
+#   @coffeeshop = Coffeeshop.find(params[:id])
+#   erb :'/coffeeshops/show'
+# end
+
+post '/coffeeshop' do
+  @coffeeshop = Coffeeshop.new(search(params['location']))
+  erb :'/coffeeshops/show', layout: false
 end
